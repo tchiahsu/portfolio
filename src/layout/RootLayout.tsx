fea9b8fd2aa01df17ead6a5fcf19e4c4a1217b01
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-08 16:44:08 -0500

    added mobile hamburger menu bar

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,18 +1,27 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Left from "../components/Left";
+import Right from "../components/Right";
 
 type RootLayoutProps = {
     children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <div>
-            <Header />
-            <main>
-                {children}
-            </main>
-            <Footer />
-        </div>
-    )
+	return (
+		<div className="min-h-screen flex flex-col">
+			{/* Portfolio Header */}
+			<Header />
+
+			{/* Main Content */}
+			<main className="bg-pink-500">
+				{children}
+			</main>
+
+			{/* Social Media Links */}
+			<Left />
+
+			{/* Email Address */}
+			<Right />
+		</div>
+	)
 }
