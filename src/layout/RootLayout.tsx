8f9974bf2b7ad6a6b586b2cccf7c2ab9adc9a288
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-09 18:24:50 -0500

    added panel for project description

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -19,9 +19,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
 			{/* Social Media Links */}
 			<Left />
-
-			{/* Email Address */}
-			{/* <Right /> */}
 		</div>
 	)
 }
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,25 +1,49 @@
 import Card from "../components/ui/card";
-import greenTrack from "../assets/projects/green_track.png";
+import greenTrack from "../assets/projects/microgreens.png";
 import pokeDuel from "../assets/projects/pokeduel.png";
 import spotifyApp from "../assets/projects/spotify.png";
 import adventureGame from "../assets/projects/adventure_game.png";
+import greenTrack2 from "../assets/projects/microgreens2.png";
+import pokeDuel2 from "../assets/projects/pokeduel2.png";
+import spotifyApp2 from "../assets/projects/spotify2.png";
 
 const lProjects = [
-  { src: greenTrack, title: "GreenTrack",
+  { src: greenTrack,
+    title: "GreenTrack",
     description: "It is a web-based application designed for microgreen farms to manage day-to-day operations.",
-    skills: ["Python", "TypeScript", "MySQL", "FastAPI", "React.js"] },
+    skills: ["Python", "TypeScript", "MySQL", "FastAPI", "React.js"],
+    src2: greenTrack2,
+    about: "This web application is a microgreen order management system tailored to Boston Microgreens’ operations. It provides its users all the functionality needed to manage their daily operations such as: recording restaurant orders, managing product offerings, storing crop growth information, tracking delivery records as well as restaurant information.",
+    github: "https://github.com/tchiahsu/microgreen_app",
+    website: ""
+  },
   { src: spotifyApp, title: "Spotify App",
     description: "A web application to visualize personalized Spotify data using the Spotify Web API.",
-    skills: ["TypeScript", "Next.js", "React.js"] }
+    skills: ["TypeScript", "Next.js", "React.js"],
+    src2: spotifyApp2,
+    about: "A personal 'Spotify Wrapped' clone. Sign in with your own Spotify account, then view your top artists, tracks, genres, and listening trends. This project uses OAuth 2.0 + PKCE and calls the Spotify Web API directly from the client. No server is required.",
+    github: "https://github.com/tchiahsu/spotify-wrapped-clone",
+    website: ""
+  }
 ]
 
 const rProjects = [
   { src: pokeDuel, title: "PokeDuel",
     description: "A full-stack turn-based Pokemon battle game inspired by Nintendo 64's Pokemon Stadium.",
-    skills: ["TypeScript", "React", "Socket.IO", "Node.js", "Express.js"] },
+    skills: ["TypeScript", "React", "Socket.IO", "Node.js", "Express.js"],
+    src2: pokeDuel2,
+    about: "A full-stack turn-based Pokemon battle game inspired by Nintendo 64's Pokemon Stadium. It features real-time 1-on-1 battles, team selection, type advantages, and 2D battle animations. Its designed to deliver a competitive multiplayer experience in the browser.",
+    github: "https://github.com/tchiahsu/pokeduel",
+    website: "https://pokeduel-gbbx.onrender.com/#/"
+  },
   { src: adventureGame, title: "Adventure Game Engine",
     description: "An object-oriented game engine that allows players to explore a dynamic world and interact with its environment.",
-    skills: ["Java", "Java Swing", "Maven"] }
+    skills: ["Java", "Java Swing", "Maven"],
+    src2: adventureGame,
+    about: "Interactive Adventure Game is an object-oriented game engine that lets players explore a dynamic world, interact with items, solve puzzles, and battle monsters. Built using the Model-View-Controller (MVC) architecture, the project focuses on a clean design, flexibility, and scalability - demonstrating core software engineering principles while being a fun and interactive game.",
+    github: "https://github.com/tchiahsu/interactive-adventure-game",
+    website: ""
+  }
 ]
 
 export default function Project() {
@@ -35,14 +59,14 @@ export default function Project() {
         {/* Left Column Project Cards */}
         <div className="flex flex-col gap-4 flex-1">
           {lProjects.map((p) => (
-            <Card src={p.src} title={p.title} description={p.description} skills={p.skills} />
+            <Card src={p.src} title={p.title} description={p.description} skills={p.skills} src2={p.src2} about={p.about} gh={p.github} web={p.website} />
           ))}
         </div>
 
         {/* Right Column */}
         <div className="flex flex-col gap-4 flex-1">
           {rProjects.map((p) => (
-            <Card src={p.src} title={p.title} description={p.description} skills={p.skills} />
+            <Card src={p.src} title={p.title} description={p.description} skills={p.skills} src2={p.src2} about={p.about} gh={p.github} web={p.website} />
           ))}
         </div>
 
