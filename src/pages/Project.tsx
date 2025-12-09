03a10416bc084de341ec4f416996ed3d2b2bb472
Tony Hsu Tai <hsutai.c@gmail.com>
2025-12-08 23:14:04 -0500

    Modified the left and right information panels

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,7 +1,15 @@
 export default function Project() {
   return (
-    <div className="flex flex-col justify-center items-center px-50 bg-pink-400">
-    Projects
+    <div className="flex flex-col my-30">
+      {/* Section Title */}
+      <div className="flex justify-start text-[#007BFF] text-4xl md:text-6xl font-semibold tracking-tight">
+        Projects.
+      </div>
+
+      {/* Project Cards */}
+      <div>
+        
+      </div>
     </div>
   )
 }
