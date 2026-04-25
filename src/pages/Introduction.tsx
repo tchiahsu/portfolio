73ea4f036f79b37fe1b1c488f32a22d632ce1093
Tony Hsu Tai <hsutai.c@gmail.com>
2026-04-25 19:20:13 -0400

    fixed spacing

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -3,7 +3,7 @@ import SocialLinks from "../components/ui/Social";
 
 export default function Introduction() {
   return (
-    <div id="home" className="pt-46 mb-12 flex flex-col items-center w-full">
+    <div id="home" className="pt-20 sm:pt-46 mb-12 flex flex-col items-center w-full">
       <Panel />
       <SocialLinks />
     </div>
